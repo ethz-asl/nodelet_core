eec59c34ebb19c92a0cf014d4101e78281ee23e2
Christian Gehring <gehrinch@ethz.ch>
2015-01-12 15:48:15 +0100

    Added unload to nodelet manager

--- nodelet/src/nodelet.cpp
+++ nodelet/src/nodelet.cpp
@@ -273,6 +273,26 @@ void shutdownCallback(XmlRpc::XmlRpcValue& params, XmlRpc::XmlRpcValue& result)
   result = ros::xmlrpc::responseInt(1, "", 0);
 }
 
+// Shutdown can be triggered externally by an XML-RPC call, this is how "rosnode kill"
+// works. When shutting down a "nodelet load" we always want to unload the nodelet
+// before shutting down our ROS comm channels, so we override the default roscpp
+// handler for a "shutdown" XML-RPC call.
+void shutdownCallbackManager(XmlRpc::XmlRpcValue& params, XmlRpc::XmlRpcValue& result)
+{
+  int num_params = 0;
+  if (params.getType() == XmlRpc::XmlRpcValue::TypeArray)
+    num_params = params.size();
+  if (num_params > 1)
+  {
+    std::string reason = params[1];
+    ROS_WARN("Shutdown request received. Reason: [%s]", reason.c_str());
+    request_shutdown = 1;
+    ros::requestShutdown();
+  }
+
+  result = ros::xmlrpc::responseInt(1, "", 0);
+}
+
 /* ---[ */
 int
   main (int argc, char** argv)
@@ -290,8 +310,18 @@ int
   if (command == "manager")
   {
     ros::init (argc, argv, "manager");
+    //signal(SIGINT, nodeletLoaderSigIntHandler);
+    ros::XMLRPCManager::instance()->unbind("shutdown");
+    ros::XMLRPCManager::instance()->bind("shutdown", shutdownCallbackManager);
+
     nodelet::Loader n;
     ros::spin ();
+    std::vector<std::string> nodelets =  n.listLoadedNodelets();
+    std::vector<std::string>::iterator it = nodelets.begin();
+    for (; it != nodelets.end(); ++it)
+    {
+    	n.unload(*it);
+    }
   }
   else if (command == "standalone")
   {
